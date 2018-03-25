a2c3d4e266b00757522e34dc0cdc16bd188ab69a
Vinh Vo <vinh.vo459@gmail.com>
2018-03-25 11:35:31 +0700

    First Flutter App
    learning ListView, Navigator

---
+++ startup_namer/lib/main.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Startup Name Generator',
+      theme: new ThemeData(
+        primaryColor: Colors.white
+      ),
+      home: new RandomWords(),
+    );
+  }
+}
+
+// 
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold (
+      appBar: new AppBar(
+        title: new Text('Startup Name Generator'),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved,),
+          new IconButton(icon: new Icon(Icons.lock), onPressed: _pushSaved,)
+        ],
+      ),
+        body: _buildSuggestions(),
+    );
+  }
+
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+  final _saved = new Set<WordPair>();
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        // Add a one-pixel-high divider widget before each row in theListView.
+        if (i.isOdd) return new Divider();
+        final index = i ~/ 2;
+        // If you've reached the end of the available word pairings...
+        if (index >= _suggestions.length) {
+          // ...then generate 10 more and add them to the suggestions list.
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      }
+    );
+  }
+  
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+        trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context) {
+
+          final tiles = _saved.map(
+            (pair) {
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase, 
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+
+          final divided = ListTile
+            .divideTiles(
+              context: context,
+              tiles: tiles
+            )
+            .toList();
+
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text("Saved Suggestion"),
+            ),
+            body: new ListView(children: divided),
+          );
+
+        }
+      ),
+    );
+  }
+}
